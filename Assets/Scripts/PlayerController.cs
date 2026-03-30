27fa6bd8953cad4db803f89a617545499e0b341f
Liisa Davydov <liisamuttonen@gmail.com>
2026-03-30 15:25:17 +0300

    Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float movementSpeed;
+
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        HandlePalyerMovement();
+    }
+
+    private void HandlePalyerMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        movement = Vector2.ClampMagnitude(movement, 1.0f);
+        _rb.linearVelocity = movement * movementSpeed;
+    }
+}
